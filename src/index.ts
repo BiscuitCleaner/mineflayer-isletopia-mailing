cbda8820641ed2032b5931530b6b96c339aba61d
BiscuitCleaner <bobpopreal@outlook.com>
2023-03-15 01:32:35 +0800

    qwq

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import mineflayer from 'mineflayer'
 import mcData from 'minecraft-data'
 import { Item } from 'prismarine-item'
 
+interface Options{
+    items: string[]
+}
 
 
 declare module 'mineflayer' {
@@ -9,6 +12,7 @@ declare module 'mineflayer' {
         registry: mcData.IndexedData
         mailing: {
             mail: (username: string,message: string, items: string[]) => void
+            options: Options
         }
     }
 
@@ -21,36 +25,37 @@ declare module 'mineflayer' {
 export function plugin(bot: mineflayer.Bot){
     // @ts-ignore
     bot.mailing={}
-
+    bot.mailing.options.items = []
     bot.mailing.mail = (username: string, message: string, items: string[]) => {
         if (bot.currentWindow) return
         bot.chat(`/mail ${username} ${message}`)
-
-        bot.once('windowOpen', async ( window ) => {
-            if (!window.title.startsWith('{"text":"放入送给')) {
-                bot.emit('mailing_error', new Error("Not Mail GUI"))
-                // @ts-ignore
-                window.close()
-                return
-            }
-
-            let item = bot.inventory.items().filter(item=>items.includes(item.name))[0]
-
-            if (!item){
-                return
-            }
-            bot.transfer({
-                window: window,
-                itemType: item.type,
-                metadata: item.metadata,
-                count: 576,
-                sourceStart: 9,
-                sourceEnd: 26,
-                destStart: 0,
-                destEnd: 8,
-            })
+        bot.mailing.options.items = items
+        
+    }
+    bot.once('windowOpen', async ( window ) => {
+        if (!window.title.startsWith('{"text":"放入送给')) {
+            bot.emit('mailing_error', new Error("Not Mail GUI"))
             // @ts-ignore
             window.close()
+            return
+        }
+
+        let item = bot.inventory.items().filter(item=>items.includes(item.name))[0]
+
+        if (!item){
+            return
+        }
+        bot.transfer({
+            window: window,
+            itemType: item.type,
+            metadata: item.metadata,
+            count: 576,
+            sourceStart: 9,
+            sourceEnd: 26,
+            destStart: 0,
+            destEnd: 8,
         })
-    }
+        // @ts-ignore
+        window.close()
+    })
 }
